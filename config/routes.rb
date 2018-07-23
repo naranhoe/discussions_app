5980f2282dc9fd36aa06497d5bf170261b27104e
naranhoe <j.naranjojr@gmail.com>
2018-07-23 16:35:03 -0400

    Add id's to models that correspond to one another

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
-  resources :discussions
+  resources :channels
+  resources :discussions do
+    resources :replies
+  end
+
   root "discussions#index"
 
   devise_for :users, controllers: { registrations: 'registrations' }
